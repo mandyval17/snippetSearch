c8694d6e8bd2ec908d597bdce9ec5a280157b295
mandyval17 <mayurvalecha2603@gmail.com>
2023-06-21 09:59:41 +0530

    finished overall

---
+++ controllers/getSnippetByKeyword.js
@@ -0,0 +1,27 @@
+const usernameschema=require("../models/usernameSchema")
+const snippetschema=require("../models/snippetSchema")
+
+
+
+
+const getSnippetByKeyword=async(req,res,next)=>{
+    const {username,input}=req.body;
+    try {
+        const data=await usernameschema.findOne({username})
+        let ans=[]
+        for(let i=0;i<data.snippets.length;i++){
+            const task=await snippetschema.findOne({_id:data.snippets[i].id})
+            const contains=task.keyword.startsWith(input)
+            if(contains){
+                ans.push(task)
+            }
+        }
+        if(ans.length)  return res.json({ans})
+        else return res.json({msg:"invalid keyword"})
+    } catch (error) {
+        next(error)
+    }   
+}
+
+
+module.exports={getSnippetByKeyword}
--- routers/routers.js
+++ routers/routers.js
@@ -4,6 +4,8 @@ const { addSnippet } = require("../controllers/addSnippet")
 const { addUser } = require("../controllers/addUser")
 const { getSnippetByCategoryPublic,getSnippetByCategoryPrivate } = require("../controllers/getByCategory")
 const { updateSnippet } = require("../controllers/updateSnippet")
+const { getSnippetId,getSnippetById } = require("../controllers/getSnippetsById")
+const { getSnippetByKeyword } = require("../controllers/getSnippetByKeyword")
 
 
 router.route("/add/username").post(addUser)
@@ -11,5 +13,8 @@ router.route("/add/snippet").post(addSnippet)
 router.route("/update/snippet").post(updateSnippet)
 router.route("/get/public").post(getSnippetByCategoryPublic)
 router.route("/get/private").post(getSnippetByCategoryPrivate)
+router.route("/get/Id").post(getSnippetId)
+router.route("/get/byId").post(getSnippetById)
+router.route("/get/byKeyword").post(getSnippetByKeyword)
 
 module.exports=router
